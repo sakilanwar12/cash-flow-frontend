8e628cd9ddf1dbe4e742892709cde2a2d88694fa
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-08-21 00:00:05 +0600

    feat: add AuthBackground component and integrate it into RegisterPage

---
+++ app/routes/auth/register/register.tsx
@@ -0,0 +1,7 @@
+import AuthBackground from "~/components/static/AuthBackground";
+
+function RegisterPage() {
+  return <AuthBackground>RegisterPage</AuthBackground>;
+}
+
+export default RegisterPage;
